8366b50fe10ffc94d2af34ba4968e0e2ce70d2fb
Phineas Huang <Phineas.Huang@garmin.com>
2018-06-11 17:53:58 +0800

    Fix issue of client

--- ClientDemo/ClientDemo/ViewController.m
+++ ClientDemo/ClientDemo/ViewController.m
@@ -25,14 +25,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)actionRin:(id)sender {
-}
-- (IBAction)actionRun:(id)sender {
-}
-- (IBAction)actionRun:(id)sender {
-}
-- (IBAction)actionRun:(id)sender {
-}
 
 - (IBAction)actionGet:(id)sender {
     [self testGet];
